93620e2647fc606674832e62167078e016d8cf81
ayushmakafle <amikafle1@gmail.com>
2024-05-02 16:18:22 +0545

    bid amount

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import LandingPage from "./components/pages/LandingPage";
 import ProposalPage from "./components/pages/ProposalPage";
+import CreateAuctionPage from "./components/pages/CreateAuction";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<LandingPage />} />
                     <Route path="/proposals" element={<ProposalPage />} />
+                    <Route path="/create-auction" element={<CreateAuctionPage />} />
                 </Routes>
             </BrowserRouter>
         </div>
