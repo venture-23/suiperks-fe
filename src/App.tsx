fc04201e2de6414fb0a8222baef92cee2ffd5e99
saajand <saajanduwal2000@gmail.com>
2024-05-08 13:55:43 +0545

    Add Auction history page with necessary API calls

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import LandingPage from "./components/pages/LandingPage";
 import ProposalPage from "./components/pages/ProposalPage";
 import CreateAuctionPage from "./components/pages/CreateAuction";
 import UserDashboard from "./components/pages/UserDashboard";
+import AuctionHistoryPage from "./components/pages/AuctionHistoryPage";
 
 function App() {
     return (
@@ -12,10 +13,11 @@ function App() {
             <BrowserRouter>
                 <Navbar />
                 <Routes>
-                    <Route path="/" element={<LandingPage />} />
                     <Route path="/proposals" element={<ProposalPage />} />
                     <Route path="/admin" element={<CreateAuctionPage />} />
                     <Route path="/user-dashboard" element={<UserDashboard />} />
+                    <Route path="/history" element={<AuctionHistoryPage />} />
+                    <Route path="/" element={<LandingPage />} />
                 </Routes>
             </BrowserRouter>
         </div>
