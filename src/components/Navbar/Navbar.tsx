1d48a4a54aae0a330bc4bb80dd3228001688109f
ayushmakafle <amikafle1@gmail.com>
2024-05-20 13:40:06 +0545

    display EthenaDAO in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
-import { ConnectButton, useWallet } from "@suiet/wallet-kit";
+import { ConnectButton, useAccountBalance, useWallet } from "@suiet/wallet-kit";
 import "./Navbar.scss";
 import { useAppContext } from "../../context/AppContext";
 import ActiveNFTDropDown from "../ActiveNFTDropDown";
 
 import MenuImg from "../../assets/images/menu.png";
 import CloseImg from "../../assets/images/crossIcon.png";
+import UserImg from "../../assets/images/usericon.png";
+import CopyIcon from "../../assets/images/copyIcon.png";
 
 const Navbar = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const wallet = useWallet();
     const { treasuryBalance } = useAppContext();
+    const [copyTooltip, setCopyTooltip] = useState("Copy wallet address");
+    const { balance: walletBalance = 0n } = useAccountBalance();
 
     const formatBalance = (balance: number | undefined): string => {
         if (typeof balance !== "number") {
@@ -22,9 +26,24 @@ const Navbar = () => {
         return formattedBalance.toFixed(2);
     };
 
+    const formatWalletBalance = (balance: number | bigint | undefined): string => {
+        if (typeof balance === "undefined") {
+            return "0.00";
+        }
+
+        const formattedWalletBalance = Number(balance) / 10 ** 9;
+        return formattedWalletBalance.toFixed(2);
+    };
+
     const [menuOpen, setMenuOpen] = useState(false);
+    const [userDropdownOpen, setUserDropdownOpen] = useState(false);
+
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
+    };
+
+    const toggleUserDropdown = () => {
+        setUserDropdownOpen(!userDropdownOpen);
     };
 
     const mobileNavigate = (url: string) => {
@@ -32,15 +51,30 @@ const Navbar = () => {
         setMenuOpen(false);
     };
 
+    const copyToClipboard = (text: string) => {
+        navigator.clipboard
+            .writeText(text)
+            .then(() => {
+                setCopyTooltip("Copied");
+                setTimeout(() => {
+                    setCopyTooltip("Copy wallet address");
+                }, 5000);
+            })
+            .catch((error) => {
+                console.error("Error copying to clipboard: ", error);
+            });
+    };
+
     return (
         <>
             <div className="navbar">
                 <div className="navbar-contents">
                     <div className="logos-and-links">
+                        <NavLink to="/" className="font-bold text-xl" style={{ fontFamily: "Capriola, sans-serif" }}>
+                            EthenaDAO
+                        </NavLink>
+
                         <div className="nav-links ">
-                            <NavLink to="/" className={`nav-link ${location.pathname === "/" ? "active" : ""}`}>
-                                Home
-                            </NavLink>
                             <NavLink
                                 to="/proposals"
                                 className={`nav-link ${location.pathname === "/proposals" ? "active" : ""}`}
@@ -67,18 +101,45 @@ const Navbar = () => {
                     </div>
 
                     <div className="right-section">
-                        {wallet.connected && (
-                            <NavLink
-                                to="/user-dashboard"
-                                className={`nav-link dashboard ${location.pathname === "/user-dashboard" ? "active" : ""}`}
-                            >
-                                Dashboard
-                            </NavLink>
+                        {wallet.connected ? (
+                            <>
+                                <NavLink
+                                    to="/user-dashboard"
+                                    className={`nav-link dashboard ${location.pathname === "/user-dashboard" ? "active" : ""}`}
+                                >
+                                    Dashboard
+                                </NavLink>
+                                <ActiveNFTDropDown />
+                                <div className="relative">
+                                    <img
+                                        src={UserImg}
+                                        className="w-[30px] h-[30px] cursor-pointer"
+                                        onClick={toggleUserDropdown}
+                                    />
+                                    {userDropdownOpen && (
+                                        <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-lg shadow-lg p-4">
+                                            <div className="flex justify-around font-bold">
+                                                <div>
+                                                    {wallet.address
+                                                        ? `${wallet.address.substring(0, 4)}...${wallet.address.substring(wallet.address.length - 4)}`
+                                                        : "Address not available"}
+                                                </div>
+                                                <div
+                                                    onClick={() => wallet.address && copyToClipboard(wallet.address)}
+                                                    title={copyTooltip}
+                                                >
+                                                    <img src={CopyIcon} className="h-6" style={{ cursor: "pointer" }} />
+                                                </div>
+                                            </div>
+
+                                            <div>Balance: {formatWalletBalance(walletBalance)} SUI</div>
+                                        </div>
+                                    )}
+                                </div>
+                            </>
+                        ) : (
+                            <ConnectButton>Connect Wallet</ConnectButton>
                         )}
-
-                        <ActiveNFTDropDown />
-
-                        <ConnectButton>Connect Wallet</ConnectButton>
                     </div>
                 </div>
             </div>
@@ -100,8 +161,12 @@ const Navbar = () => {
                     {menuOpen && <div className="overlay" onClick={toggleMenu}></div>}
                     {menuOpen && (
                         <div className="dropdown-menu">
-                            <div onClick={() => mobileNavigate("/")}>
-                                <div className="mobile-nav-link">Home</div>
+                            <div
+                                onClick={() => mobileNavigate("/")}
+                                className="font-bold text-xl text-black py-3"
+                                style={{ fontFamily: "Capriola, sans-serif" }}
+                            >
+                                EthenaDAO
                             </div>
                             <div onClick={() => mobileNavigate("/proposals")}>
                                 <div className="mobile-nav-link">Proposals</div>
@@ -112,10 +177,20 @@ const Navbar = () => {
                             <div onClick={() => mobileNavigate("/history")}>
                                 <div className="mobile-nav-link">History</div>
                             </div>
+                            <div className="text-black py-3">
+                                <div>Treasury: {formatBalance(treasuryBalance)} SUI</div>
+                            </div>
+
                             {wallet.connected && (
-                                <div onClick={() => mobileNavigate("/user-dashboard")}>
-                                    <div className="mobile-nav-link">Dashboard</div>
-                                </div>
+                                <>
+                                    <div onClick={() => mobileNavigate("/user-dashboard")}>
+                                        <div className="mobile-nav-link">Dashboard</div>
+                                    </div>
+
+                                    <div className="text-black py-3">
+                                        <ActiveNFTDropDown />
+                                    </div>
+                                </>
                             )}
                         </div>
                     )}
