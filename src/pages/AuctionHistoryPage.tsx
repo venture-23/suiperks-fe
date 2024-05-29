6d799cd858d947dfaead9146f711140fe1301e58
ayushmakafle <amikafle1@gmail.com>
2024-05-29 19:36:39 +0545

    Link to NFT details page

--- src/pages/AuctionHistoryPage.tsx
+++ src/pages/AuctionHistoryPage.tsx
@@ -39,55 +39,57 @@ const AuctionHistoryPage = () => {
                 <div className="flex flex-col items-center gap-6">
                     {historyData?.length > 0 &&
                         historyData.map((auctionData) => (
-                            <div className="box-shadow max-w-[800px] w-full mx-auto md:mx-0 flex md:flex-row flex-col items-start rounded-md gap-4 p-4 overflow-auto">
-                                <div className="w-[100px] h-[100px] flex-shrink-0">
-                                    <img
-                                        src={auctionData.nftImage}
-                                        alt={auctionData.title}
-                                        className="w-full h-full object-contain bg-[rgba(255,255,255,0.5)] rounded-lg"
-                                    />
-                                </div>
+                            <Link to={`/nft/${auctionData.nftId}`} className="flex gap-8">
+                                <div className="box-shadow md:w-[800px] sm:w-[500px] w-[350px] mx-auto md:mx-0 flex md:flex-row flex-col items-start rounded-md gap-4 p-4 overflow-auto">
+                                    <div className="w-[100px] h-[100px] flex-shrink-0">
+                                        <img
+                                            src={auctionData.nftImage}
+                                            alt={auctionData.title}
+                                            className="w-full h-full object-contain bg-[rgba(255,255,255,0.5)] rounded-lg"
+                                        />
+                                    </div>
 
-                                <div className="flex flex-col gap-1">
-                                    <div className="text-xl text-[#1c0971] font-bold">{auctionData.title}</div>
-                                    <div className=" text-sm">{auctionData.description}</div>
-                                    <div>
-                                        NFT:&nbsp;
-                                        <span className="name text-[#1c0971] my-2 md:text-2xl text-lg font-bold">
-                                            {auctionData.nftName}
-                                        </span>
-                                    </div>
-                                    <div className="md:text-lg text-sm">
-                                        NFT Description: {auctionData.nftDescription}
-                                    </div>
-                                    <div>
-                                        NFT Id:{" "}
-                                        <Link
-                                            to={`${SUI_EXPLORER_URL}/object/${auctionData.nftId}`}
-                                            target="_blank"
-                                            className="underline"
-                                        >
-                                            {auctionData.nftId.slice(0, 6)}...{auctionData.nftId.slice(-6)}
-                                        </Link>
-                                    </div>
-                                    <div>
-                                        Winner:{" "}
-                                        <Link
-                                            to={`${SUI_EXPLORER_URL}/account/${auctionData.highestBidder}`}
-                                            target="_blank"
-                                            className="underline"
-                                        >
-                                            {" "}
-                                            {auctionData?.highestBidder?.slice(0, 6)}...
-                                            {auctionData?.highestBidder?.slice(-6)}
-                                        </Link>
-                                    </div>
-                                    <div className="text-xl">
-                                        Bidding Price:{" "}
-                                        <span className="font-bold">{auctionData.amount * 10 ** -9} SUI</span>
+                                    <div className="flex flex-col gap-1">
+                                        <div className="text-xl text-[#1c0971] font-bold">{auctionData.title}</div>
+                                        <div className=" text-sm">{auctionData.description}</div>
+                                        <div>
+                                            NFT:&nbsp;
+                                            <span className="name text-[#1c0971] my-2 md:text-2xl text-lg font-bold">
+                                                {auctionData.nftName}
+                                            </span>
+                                        </div>
+                                        <div className="md:text-lg text-sm">
+                                            NFT Description: {auctionData.nftDescription}
+                                        </div>
+                                        <div>
+                                            NFT Id:{" "}
+                                            <Link
+                                                to={`${SUI_EXPLORER_URL}/object/${auctionData.nftId}`}
+                                                target="_blank"
+                                                className="underline"
+                                            >
+                                                {auctionData.nftId.slice(0, 6)}...{auctionData.nftId.slice(-6)}
+                                            </Link>
+                                        </div>
+                                        <div>
+                                            Winner:{" "}
+                                            <Link
+                                                to={`${SUI_EXPLORER_URL}/account/${auctionData.highestBidder}`}
+                                                target="_blank"
+                                                className="underline"
+                                            >
+                                                {" "}
+                                                {auctionData?.highestBidder?.slice(0, 6)}...
+                                                {auctionData?.highestBidder?.slice(-6)}
+                                            </Link>
+                                        </div>
+                                        <div className="text-xl">
+                                            Bidding Price:{" "}
+                                            <span className="font-bold">{auctionData.amount * 10 ** -9} SUI</span>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </Link>
                         ))}
                 </div>
             </div>
